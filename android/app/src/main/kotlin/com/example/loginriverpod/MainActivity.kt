ccf6deff2fb2b001df8828aed5833fae8f39a475
münevver <munvververim@gmail.com>
2024-01-26 19:19:54 +0300

    Dosyaları ekle

---
+++ android/app/src/main/kotlin/com/example/loginriverpod/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.loginriverpod
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:loginriverpod/features/user/repositories/auth_repository.dart';
+import 'package:loginriverpod/screens/dashboard/dashboard_screen.dart';
+import 'package:loginriverpod/screens/login/login_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const ProviderScope(child: MyApp()));
+}
+
+class MyApp extends HookConsumerWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ref
+          .watch(
+        getIsAuthenticatedProvider,
+      )
+          .when(
+        data: (bool isAuthenticated) =>
+        isAuthenticated ? const DashboardScreen() : const LoginScreen(),
+        loading: () {
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        },
+        error: (error, stacktrace) => const LoginScreen(),
+      ),
+      routes: {
+        "Home": (context) => const DashboardScreen(),
+        "Login": (context) => const LoginScreen(),
+      },
+    );
+  }
+}
+
+
